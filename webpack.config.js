4bf5f859fd349a8340bf9ccf32bddbe305c3e5a0
Alex <147102432+sobol4156@users.noreply.github.com>
2024-05-26 17:58:36 +0300

    configured webpack

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+  ],
+  devServer: {
+    static: { directory: path.join(__dirname, "dist") },
+    compress: true,
+    port: 2228,
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+};
