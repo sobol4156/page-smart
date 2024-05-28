869a6d601924e2930e1b61613229bfcbd1eefeb8
Alex <147102432+sobol4156@users.noreply.github.com>
2024-05-28 14:55:18 +0300

    lags comp

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -29,6 +29,7 @@ const Cart = () => {
   const handleToggleCompany = () => {
     setShowCompany(true);
   };
+
   const handleTogglEemployee = () => {
     setShowCompany(false);
   };
@@ -56,113 +57,120 @@ const Cart = () => {
         </div>
       </header>
       <main className={`main ${!showCompany ? "active" : ""}`}>
-        <div className="main__content">
-          <div className="main__images">
-            <div className="main__images-img">
-              <img src={photo1} alt="" />
+        {!showCompany && (
+          <div>
+          <div className="main__content">
+            <div className="main__images">
+              <div className="main__images-img">
+                <img src={photo1} alt="" />
+              </div>
+              <div className="main__images-btn">
+                <button>ДОБАВИТЬ В КОНТАКТЫ</button>
+              </div>
             </div>
-            <div className="main__images-btn">
-              <button>ДОБАВИТЬ В КОНТАКТЫ</button>
-            </div>
-          </div>
-          <div className="main__text">
-            <div className="main__logo">
-              <span>Logo</span>
-            </div>
-            <div className="main__border"></div>
-            <div className="main__info">
-              <div className="main__header">
-                <div className="main__FIO">
-                  <span>
-                    Иванов <br /> Иван Петрович
-                  </span>
+            <div className="main__text">
+              <div className="main__logo">
+                <span>Logo</span>
+              </div>
+              <div className="main__border"></div>
+              <div className="main__info">
+                <div className="main__header">
+                  <div className="main__FIO">
+                    <span>
+                      Иванов <br /> Иван Петрович
+                    </span>
+                  </div>
+                  <div className="main__post">
+                    <span>Директор</span>
+                  </div>
                 </div>
-                <div className="main__post">
-                  <span>Директор</span>
+                <div className="main__info-more">
+                  <div className="main__phone inf">
+                    <img src={smartphone} alt="" />{" "}
+                    <span>+7 (000) 000-00-00</span>
+                  </div>
+                  <div className="main__phone inf">
+                    <img src={phone} alt="" /> <span>+7 (000) 000-00-00</span>
+                  </div>
+                  <div className="main__phone inf">
+                    <img src={phone} alt="" /> <span>+7 (000) 000-00-00</span>
+                  </div>
+                  <div className="main__email inf">
+                    <img src={email} alt="" /> <span>Почта</span>
+                  </div>
+                  <div className="main__website inf">
+                    <img src={websait} alt="" /> <span>Личный сайт</span>
+                  </div>
+                  <div className="main__address inf">
+                    <img src={address} alt="" /> <span>Адрес</span>
+                  </div>
+                  <div className="main__BD inf">
+                    <img src={bd} alt="" /> <span>Дата рождения</span>
+                  </div>
+                  <div className="main__rank inf">
+                    <img src={rank} alt="" /> <span>Звание, учёная степень</span>
+                  </div>
                 </div>
               </div>
+              
+            </div>
+            
+          </div>
+                      <div className="main__border-bottom"></div>
+            <div className="main__social">
+              <div className="main__social-icons">
+                <div className="social-item">
+                  <img src={telegram} alt="" />
+                </div>
+                <div className="social-item">
+                  <img src={whatsup} alt="" />
+                </div>
+                <div className="social-item">
+                  <img src={viber} alt="" />
+                </div>
+                <div className="social-item">
+                  <img src={instagram} alt="" />
+                </div>
+                <div className="social-item">
+                  <img src={vk} alt="" />
+                </div>
+                <div className="social-item">
+                  <img src={facebook} alt="" />
+                </div>
+                <div className="social-item">
+                  <img src={youtube} alt="" />
+                </div>
+                <div className="social-item">
+                  <img src={ok} alt="" />
+                </div>
+                <div className="social-item">
+                  <img src={dzen} alt="" />
+                </div>
+                <div className="social-item">
+                  <img src={tiktok} alt="" />
+                </div>
+                <div className="social-item">
+                  <img src={linkedin} alt="" />
+                </div>
+              </div>
+            </div>
+          </div>
 
-              <div className="main__info-more">
-                <div className="main__phone inf">
-                  <img src={smartphone} alt="" />{" "}
-                  <span>+7 (000) 000-00-00</span>
-                </div>
-                <div className="main__phone inf">
-                  <img src={phone} alt="" /> <span>+7 (000) 000-00-00</span>
-                </div>
-                <div className="main__phone inf">
-                  <img src={phone} alt="" /> <span>+7 (000) 000-00-00</span>
-                </div>
-                <div className="main__email inf">
-                  <img src={email} alt="" /> <span>Почта</span>
-                </div>
-                <div className="main__website inf">
-                  <img src={websait} alt="" /> <span>Личный сайт</span>
-                </div>
-                <div className="main__address inf">
-                  <img src={address} alt="" /> <span>Адрес</span>
-                </div>
-                <div className="main__BD inf">
-                  <img src={bd} alt="" /> <span>Дата рождения</span>
-                </div>
-                <div className="main__rank inf">
-                  <img src={rank} alt="" /> <span>Звание, учёная степень</span>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="main__border-bottom"></div>
-        <div className="main__social">
-          <div className="main__social-icons">
-            <div className="social-item">
-              <img src={telegram} alt="" />
-            </div>
-            <div className="social-item">
-              <img src={whatsup} alt="" />
-            </div>
-            <div className="social-item">
-              <img src={viber} alt="" />
-            </div>
-            <div className="social-item">
-              <img src={instagram} alt="" />
-            </div>
-            <div className="social-item">
-              <img src={vk} alt="" />
-            </div>
-            <div className="social-item">
-              <img src={facebook} alt="" />
-            </div>
-            <div className="social-item">
-              <img src={youtube} alt="" />
-            </div>
-            <div className="social-item">
-              <img src={ok} alt="" />
-            </div>
-            <div className="social-item">
-              <img src={dzen} alt="" />
-            </div>
-            <div className="social-item">
-              <img src={tiktok} alt="" />
-            </div>
-            <div className="social-item">
-              <img src={linkedin} alt="" />
-            </div>
-          </div>
-        </div>
+        )}
       </main>
       <main className={`main ${showCompany ? "active" : ""}`}>
-        <div className="company__content">
-          <div className="company__header"></div>
-          <div className="company__main">
-            <div className="company__logo">
-              <div className="company__logo-title">Logo</div>
-              <div className="company__logo-text">
-                <div className="company__logo-name">Название компании</div>
-                <div className="company__logo-description">Описание компании</div>
+        {showCompany && (
+          <div className="company__content">
+            <div className="company__header"></div>
+            <div className="company__main">
+              <div className="company__logo">
+                <div className="company__logo-title">Logo</div>
+                <div className="company__logo-text">
+                  <div className="company__logo-name">Название компании</div>
+                  <div className="company__logo-description">Описание компании</div>
+                </div>
               </div>
-            </div>
-            <div className="main__info-more">
+              <div className="main__info-more">
                 <div className="main__phone inf">
                   <img src={phone} alt="" /> <span>+7 (000) 000-00-00</span>
                 </div>
@@ -185,46 +193,47 @@ const Cart = () => {
                   <img src={fax} alt="" /> <span>Факс</span>
                 </div>
               </div>
+            </div>
+            <div className="main__border-bottom"></div>
+            <div className="main__social">
+              <div className="main__social-icons">
+                <div className="social-item">
+                  <img src={telegram} alt="" />
+                </div>
+                <div className="social-item">
+                  <img src={whatsup} alt="" />
+                </div>
+                <div className="social-item">
+                  <img src={viber} alt="" />
+                </div>
+                <div className="social-item">
+                  <img src={instagram} alt="" />
+                </div>
+                <div className="social-item">
+                  <img src={vk} alt="" />
+                </div>
+                <div className="social-item">
+                  <img src={facebook} alt="" />
+                </div>
+                <div className="social-item">
+                  <img src={youtube} alt="" />
+                </div>
+                <div className="social-item">
+                  <img src={ok} alt="" />
+                </div>
+                <div className="social-item">
+                  <img src={dzen} alt="" />
+                </div>
+                <div className="social-item">
+                  <img src={tiktok} alt="" />
+                </div>
+                <div className="social-item">
+                  <img src={linkedin} alt="" />
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="main__border-bottom"></div>
-        <div className="main__social">
-          <div className="main__social-icons">
-            <div className="social-item">
-              <img src={telegram} alt="" />
-            </div>
-            <div className="social-item">
-              <img src={whatsup} alt="" />
-            </div>
-            <div className="social-item">
-              <img src={viber} alt="" />
-            </div>
-            <div className="social-item">
-              <img src={instagram} alt="" />
-            </div>
-            <div className="social-item">
-              <img src={vk} alt="" />
-            </div>
-            <div className="social-item">
-              <img src={facebook} alt="" />
-            </div>
-            <div className="social-item">
-              <img src={youtube} alt="" />
-            </div>
-            <div className="social-item">
-              <img src={ok} alt="" />
-            </div>
-            <div className="social-item">
-              <img src={dzen} alt="" />
-            </div>
-            <div className="social-item">
-              <img src={tiktok} alt="" />
-            </div>
-            <div className="social-item">
-              <img src={linkedin} alt="" />
-            </div>
-          </div>
-        </div>
-        </div>
+        )}
       </main>
     </div>
   );
